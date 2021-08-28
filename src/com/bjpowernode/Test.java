982eb4b198ed1c5688199eac19fe464c3871bca5
ty <ty@qq.com>
2021-08-28 11:51:48 +0800

    1

--- src/com/bjpowernode/Test.java
+++ src/com/bjpowernode/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你好啊!123");
         System.out.println("1110");
+        System.out.println("hahaha");
     }
 }
